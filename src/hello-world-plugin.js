52ad4e04222c636a34a6ed724a1281a49cbcb2a2
xiongmeihui <xiongmeihui@youzan.com>
2020-05-06 20:38:48 +0800

    feat: compiler生命周期主要钩子完成

--- src/hello-world-plugin.js
+++ src/hello-world-plugin.js
@@ -115,6 +115,163 @@ class HelloWorldPlugin {
         // console.log("------------------------------------------------------");
       }
     );
+
+    /**
+     * params 用来创建 compilation
+     */
+    compiler.hooks.compile.tap("HelloWorldPlugin", (params) => {
+      console.log(11);
+      // console.log("------------------------------------------------------");
+      // console.log(params);
+      // console.log("------------------------------------------------------");
+    });
+
+    /**
+     * 初始化 compilation 的逻辑注册
+     */
+    compiler.hooks.thisCompilation.tap(
+      "HelloWorldPlugin",
+      (compilation, compilationParams) => {
+        console.log(12);
+        // console.log("------------------------------------------------------");
+        // console.log(compilation);
+        // console.log(compilationParams);
+        // console.log("------------------------------------------------------");
+      }
+    );
+
+    /**
+     * 创建 compilation 实例后触发
+     */
+    compiler.hooks.compilation.tap(
+      "HelloWorldPlugin",
+      (compilation, compilationParams) => {
+        console.log(13);
+        // console.log("------------------------------------------------------");
+        // console.log(compilation);
+        // console.log(compilationParams);
+        // console.log("------------------------------------------------------");
+      }
+    );
+
+    /**
+     * 开始分析入口文件，创建模块对象
+     * 该钩子为同步并行
+     */
+    compiler.hooks.make.tapAsync("HelloWorldPlugin", (compilation, done) => {
+      console.log(14);
+      done();
+      // console.log("------------------------------------------------------");
+      // console.log(compilation);
+      // console.log("------------------------------------------------------");
+    });
+
+    /**
+     * 完成所有模块的构建，结束编译过程
+     */
+    compiler.hooks.afterCompile.tapAsync(
+      "HelloWorldPlugin",
+      (compilation, next) => {
+        console.log(15);
+        next();
+        // console.log("------------------------------------------------------");
+        // console.log(compilation);
+        // console.log("------------------------------------------------------");
+      }
+    );
+
+    /**
+     * return false 将不会执行输出逻辑
+     */
+    compiler.hooks.shouldEmit.tap("HelloWorldPlugin", (compilation) => {
+      console.log(16);
+      // return false;
+      // console.log("------------------------------------------------------");
+      // console.log(compilation);
+      // console.log("------------------------------------------------------");
+    });
+
+    /**
+     * 在输出 assets 前调用
+     */
+    compiler.hooks.emit.tapAsync("HelloWorldPlugin", (compilation, next) => {
+      console.log(17);
+      next();
+      // console.log("------------------------------------------------------");
+      // console.log(compilation);
+      // console.log("------------------------------------------------------");
+    });
+
+    /**
+     * 每当一个 asset 被输出的时候调用，提供了对输出的 asset 资源的访问
+     */
+    compiler.hooks.assetEmitted.tapAsync(
+      "HelloWorldPlugin",
+      (file, info, next) => {
+        console.log(18);
+        // console.log("------------------------------------------------------");
+        // console.log(file);
+        // console.log(info);
+        // console.log("------------------------------------------------------");
+        next();
+      }
+    );
+
+    /**
+     * 输出 assets 完成后调用
+     */
+    compiler.hooks.afterEmit.tapAsync(
+      "HelloWorldPlugin",
+      (compilation, next) => {
+        console.log(19);
+        next();
+        // console.log("------------------------------------------------------");
+        // console.log(compilation);
+        // console.log("------------------------------------------------------");
+      }
+    );
+
+    /**
+     * 单次构建完成后被调用，参数为单次构建的详细统计信息
+     */
+    compiler.hooks.done.tapAsync("HelloWorldPlugin", (stats, next) => {
+      console.log(20);
+      next();
+      // console.log("------------------------------------------------------");
+      // console.log(stats);
+      // console.log("------------------------------------------------------");
+    });
+
+    /**
+     * 构建失败时调用
+     */
+    compiler.hooks.failed.tap("HelloWorldPlugin", (err) => {
+      console.log(21);
+      // console.log("------------------------------------------------------");
+      // console.log(err);
+      // console.log("------------------------------------------------------");
+    });
+
+    /**
+     * 单次构建的 watching 被 invalidate
+     */
+    compiler.hooks.invalid.tap("HelloWorldPlugin", (fileName, changeTime) => {
+      console.log(22);
+      // console.log("------------------------------------------------------");
+      // console.log(fileName);
+      // console.log(changeTime);
+      // console.log("------------------------------------------------------");
+    });
+
+    /**
+     * 单次构建的 watching 停止
+     */
+    compiler.hooks.watchClose.tap("HelloWorldPlugin", () => {
+      console.log(23);
+      // console.log("------------------------------------------------------");
+      // console.log(无参数);
+      // console.log("------------------------------------------------------");
+    });
   }
 }
 
